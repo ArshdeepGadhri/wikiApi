3cb611fccd021519c03f89ea6814a5e8d555f481
ArshdeepGadhri <arshdeep.gadhri@mail.utoronto.ca>
2021-05-24 18:30:07 -0400

    Removed empty indices from self.paragraphs if any

--- wiki.py
+++ wiki.py
@@ -1,6 +1,14 @@
 from bs4 import BeautifulSoup
 import requests
 
+header = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36',
+    "Upgrade-Insecure-Requests": "1", "DNT": "1",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Accept-Encoding": "gzip, deflate"
+}
+
 
 class Wikipedia:
     title: str
@@ -14,12 +22,6 @@ class Wikipedia:
         self.length = length
         self.paragraphs = []
         self.infobox = {}
-        self.headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36',
-            "Upgrade-Insecure-Requests": "1", "DNT": "1",
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-            "Accept-Language": "en-US,en;q=0.5",
-            "Accept-Encoding": "gzip, deflate"}
 
     def __str__(self) -> str:
         """
@@ -33,13 +35,20 @@ class Wikipedia:
         """
         return self.__str__()
 
+    def clean(self) -> None:
+        """
+        Removes any unnecessary data from self.paragraphs
+        """
+        while '' in self.paragraphs:
+            self.paragraphs.pop(self.paragraphs.index(''))
+
     def request(self) -> dict:
         """
         Return a dictionary containing the necessary data from a
         specific wikipedia article
         """
-        url = "https://en.wikipedia.org/wiki/" + self.title.replace(' ', '_')
-        page = requests.get(url, headers=self.headers)
+        url = "https://en.wikipedia.org/wiki/" + self.title.replace('%20', '_')
+        page = requests.get(url, headers=header)
         soup = BeautifulSoup(page.content, 'html.parser')
 
         title = soup.find(id="firstHeading")
@@ -48,17 +57,21 @@ class Wikipedia:
         for para in paragraphs[:self.length]:
             self.paragraphs.append(para.get_text().strip())
 
+        if 'Other reasons this message may be displayed:' in self.paragraphs:
+            return {'status': 0}
+
         infobox = soup.find(id='bodyContent').findAll('tr')
         for info in infobox[:30]:
             th = info.find('th')
             td = info.find('td')
             try:
-                self.infobox[th.text.strip()] = td.text.strip()
+                if th and td:
+                    self.infobox[th.text.strip()] = td.text.strip()
             except:
                 return {'status': 0}
 
         self.title = title.string.strip()
-
+        self.clean()
         return {
             'status': 1,
             "Title": self.title,
